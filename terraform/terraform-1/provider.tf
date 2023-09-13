d8a3802e9ccd87115ecfa3477ac45519bcf39d6f
Sam <51818160+Aprazor@users.noreply.github.com>
2023-09-12 23:20:58 -0700

    Delete terraform/terraform-1 directory

--- terraform/terraform-1/provider.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region     = "us-east-2"
-  access_key = "Your Key"
-  secret_key = "Your Key"
-}
-
-provider "random" {
-  version = "3.1.0"  # Use the desired version of the random provider
-}
